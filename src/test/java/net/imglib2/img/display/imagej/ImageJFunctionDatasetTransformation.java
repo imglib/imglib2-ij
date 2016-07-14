920fd41fd10181f539cf2ca735a724e3e37b07ba
cmongis <c.mongis@zmbh.uni-heidelberg.de>
2016-07-14 14:11:32 +0200

    Reformated using Eclipse IMGLIB code formatting.

--- src/test/java/net/imglib2/img/display/imagej/ImageJFunctionDatasetTransformation.java
+++ src/test/java/net/imglib2/img/display/imagej/ImageJFunctionDatasetTransformation.java
@@ -18,33 +18,32 @@ import org.junit.Test;
  *
  * @author cyril
  */
-public class ImageJFunctionDatasetTransformation {
-    
-    
-    
-    
-    @Test
-    public void transformBitDataset() {
-        
-        
-         final ImgFactory< BitType > imgFactory = new CellImgFactory< BitType >( 5 );
- 
-        // create an 3d-Img with dimensions 20x30x40 (here cellsize is 5x5x5)Ø
-        final Img< BitType > img1 = imgFactory.create( new long[]{ 20, 30, 40 }, new BitType() );
-        
-        Cursor<BitType> cursor = img1.cursor();
-        
-        cursor.reset();
-        // setting all the pixels as white
-        while(cursor.hasNext()) {
-            cursor.fwd();
-            cursor.get().set(new BitType(true));
-        }
-        
-        ImagePlus imp = ImageJFunctions.wrap(img1, "");
-        
-        Assert.assertEquals(ImagePlus.GRAY8,imp.getType());
-        Assert.assertEquals(255,imp.getStatistics().min,0);
-        
-    }
+public class ImageJFunctionDatasetTransformation
+{
+
+	@Test
+	public void transformBitDataset()
+	{
+
+		final ImgFactory< BitType > imgFactory = new CellImgFactory< BitType >( 5 );
+
+		// create an 3d-Img with dimensions 20x30x40 (here cellsize is 5x5x5)Ø
+		final Img< BitType > img1 = imgFactory.create( new long[] { 20, 30, 40 }, new BitType() );
+
+		Cursor< BitType > cursor = img1.cursor();
+
+		cursor.reset();
+		// setting all the pixels as white
+		while ( cursor.hasNext() )
+		{
+			cursor.fwd();
+			cursor.get().set( new BitType( true ) );
+		}
+
+		ImagePlus imp = ImageJFunctions.wrap( img1, "" );
+
+		Assert.assertEquals( ImagePlus.GRAY8, imp.getType() );
+		Assert.assertEquals( 255, imp.getStatistics().min, 0 );
+
+	}
 }
