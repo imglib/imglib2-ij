9ee2a55a3ae87dd8baf08de5e2a17931bcc865e1
Albert Cardona <sapristi@gmail.com>
2018-10-18 16:47:11 -0400

    IJLoader: a Loader that uses IJ.openImage to read file paths.

---
+++ src/main/java/net/imglib2/io/IJLoader.java
@@ -0,0 +1,17 @@
+package net.imglib2.io;
+
+import ij.IJ;
+import net.imglib2.img.Img;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.NumericType;
+
+public class IJLoader< T extends NumericType< T > & NativeType< T > > implements Loader< T >
+{
+
+	@Override
+	public Img< T > load( final String path )
+	{
+		return ImageJFunctions.wrap( IJ.openImage( path ) );
+	}
+}
