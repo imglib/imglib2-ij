65f2adf982a2454e9dd93316628eb9d2e9050f67
tpietzsch <tobias.pietzsch@gmail.com>
2022-11-01 09:32:02 +0100

    WIP fix pivot calculation, poc NN search (creates lots of objects)

---
+++ src/main/java/net/imglib2/kdtree/NearestNeighborSearchOnKDTree.java
@@ -0,0 +1,140 @@
+/*
+ * #%L
+ * ImgLib2: a general-purpose, multidimensional image processing library.
+ * %%
+ * Copyright (C) 2009 - 2021 Tobias Pietzsch, Stephan Preibisch, Stephan Saalfeld,
+ * John Bogovic, Albert Cardona, Barry DeZonia, Christian Dietz, Jan Funke,
+ * Aivar Grislis, Jonathan Hale, Grant Harris, Stefan Helfrich, Mark Hiner,
+ * Martin Horn, Steffen Jaensch, Lee Kamentsky, Larry Lindsey, Melissa Linkert,
+ * Mark Longair, Brian Northan, Nick Perry, Curtis Rueden, Johannes Schindelin,
+ * Jean-Yves Tinevez and Michael Zinsmaier.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imglib2.kdtree;
+
+import net.imglib2.RealLocalizable;
+import net.imglib2.Sampler;
+import net.imglib2.kdtree.FlatKDTree.KDTree;
+import net.imglib2.kdtree.FlatKDTree.KDTreeNode;
+import net.imglib2.neighborsearch.NearestNeighborSearch;
+
+/**
+ * Implementation of {@link NearestNeighborSearch} search for kd-trees.
+ * 
+ * 
+ * @author Tobias Pietzsch
+ */
+public class NearestNeighborSearchOnKDTree< T > implements NearestNeighborSearch< T >
+{
+	protected KDTree< T > tree;
+
+	protected final int n;
+
+	protected final double[] pos;
+
+	protected KDTreeNode< T > bestPoint;
+
+	protected double bestSquDistance;
+
+	public NearestNeighborSearchOnKDTree( final KDTree< T > tree )
+	{
+		n = tree.numDimensions();
+		pos = new double[ n ];
+		this.tree = tree;
+	}
+
+	@Override
+	public int numDimensions()
+	{
+		return n;
+	}
+
+	@Override
+	public void search( final RealLocalizable p )
+	{
+		p.localize( pos );
+		bestSquDistance = Double.MAX_VALUE;
+		searchNode( tree.getRoot() );
+	}
+
+	protected void searchNode( final KDTreeNode< T > current )
+	{
+		// consider the current node
+		final double distance = current.squDistanceTo( pos );
+		if ( distance < bestSquDistance )
+		{
+			bestSquDistance = distance;
+			bestPoint = current;
+		}
+
+		final double axisDiff = pos[ current.getSplitDimension() ] - current.getSplitCoordinate();
+		final double axisSquDistance = axisDiff * axisDiff;
+		final boolean leftIsNearBranch = axisDiff < 0;
+
+		// search the near branch
+		final KDTreeNode< T > nearChild = leftIsNearBranch ? current.left() : current.right();
+		final KDTreeNode< T > awayChild = leftIsNearBranch ? current.right() : current.left();
+		if ( nearChild != null )
+			searchNode( nearChild );
+
+		// search the away branch - maybe
+		if ( ( axisSquDistance <= bestSquDistance ) && ( awayChild != null ) )
+			searchNode( awayChild );
+	}
+
+	@Override
+	public Sampler< T > getSampler()
+	{
+		return bestPoint;
+	}
+
+	@Override
+	public RealLocalizable getPosition()
+	{
+		return bestPoint;
+	}
+
+	@Override
+	public double getSquareDistance()
+	{
+		return bestSquDistance;
+	}
+
+	@Override
+	public double getDistance()
+	{
+		return Math.sqrt( bestSquDistance );
+	}
+
+	@Override
+	public NearestNeighborSearchOnKDTree< T > copy()
+	{
+		final NearestNeighborSearchOnKDTree< T > copy = new NearestNeighborSearchOnKDTree< T >( tree );
+		System.arraycopy( pos, 0, copy.pos, 0, pos.length );
+		copy.bestPoint = bestPoint;
+		copy.bestSquDistance = bestSquDistance;
+		return copy;
+	}
+}
